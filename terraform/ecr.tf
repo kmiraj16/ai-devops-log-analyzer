1306d11ffbbc1a682e43d69c4bcce10fcee63d2b
Miraj Patel <234297527+kmiraj16@users.noreply.github.com>
2026-04-23 08:43:00 -0500

    chore: allow ecr force delete [skip ci]

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,7 +1,9 @@
-# Create the ECR repository
 resource "aws_ecr_repository" "app_repo" {
   name                 = "ai-log-analyzer"
   image_tag_mutability = "MUTABLE"
+  
+  # ADD THIS LINE: Tells AWS to delete the repo even if it has images
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
